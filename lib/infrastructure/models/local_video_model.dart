e2264f040bd9646b8d70aa7e41d819a30f481d81
Sergio Barreras González <sergiobg.isc@gmail.com>
2023-08-23 17:02:15 -0700

    modelo de videos creado

---
+++ lib/infrastructure/models/local_video_model.dart
@@ -0,0 +1,21 @@
+import 'package:shorts_app/domain/entities/video_post.dart';
+
+class LocalVideoModel {
+  final String name;
+  final String url;
+  final int likes;
+  final int views;
+
+  LocalVideoModel(
+      {required this.name, required this.url, this.likes = 0, this.views = 0});
+
+  factory LocalVideoModel.fromJson(Map<String, dynamic> json) =>
+      LocalVideoModel(
+          name: json['name'],
+          url: json['videoUrl'],
+          likes: json['likes'],
+          views: json['views']);
+
+  VideoPost toVideoPostEntity() =>
+      VideoPost(caption: name, videoUrl: url, likes: likes, views: views);
+}
