3a01ffae757381ecd0fb3c332ab1d98d483640ab
Sergio Barreras González <sergiobg.isc@gmail.com>
2023-08-23 17:16:54 -0700

    asignación de videos al provider

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/widgets.dart';
 import 'package:shorts_app/domain/entities/video_post.dart';
+import 'package:shorts_app/infrastructure/models/local_video_model.dart';
+import 'package:shorts_app/shared/data/local_video_post.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+//Todo: Repository, datasource
+
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
   Future<void> loadNextPage() async {
-//todo: cargar videos
+    await Future.delayed(const Duration(seconds: 2));
+
+    final List<VideoPost> newVideos = videoPosts
+        .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
+        .toList();
+
+    videos.addAll(newVideos);
+
+    initialLoading = false;
     notifyListeners();
   }
 }
