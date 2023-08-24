e1f9d9b7c13defdca5d771116b06721814dcb5b0
Sergio Barreras González <sergiobg.isc@gmail.com>
2023-08-24 16:40:17 -0700

    implementacion de repositorios y datasource

--- lib/infrastructure/repositories/video_posts_repository_impl.dart
+++ lib/infrastructure/repositories/video_posts_repository_impl.dart
@@ -2,10 +2,10 @@ import 'package:shorts_app/domain/datasources/video_post_datasource.dart';
 import 'package:shorts_app/domain/entities/video_post.dart';
 import 'package:shorts_app/domain/repositories/video_post_repository.dart';
 
-class VideoPostsRepository implements VideoPostRepository {
+class VideoPostsRepositoryImpl implements VideoPostRepository {
   final VideoPostDatasource videosDatasource;
 
-  VideoPostsRepository({required this.videosDatasource});
+  VideoPostsRepositoryImpl({required this.videosDatasource});
 
   @override
   Future<List<VideoPost>> getFavoriteVideosByUser(String user) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shorts_app/config/theme/app_theme.dart';
+import 'package:shorts_app/infrastructure/datasources/local_video_datasource_impl.dart';
+import 'package:shorts_app/infrastructure/repositories/video_posts_repository_impl.dart';
 import 'package:shorts_app/presentation/screens/discover/discover_screen.dart';
 import 'package:shorts_app/presentation/providers/discover_provider.dart';
 
@@ -11,10 +13,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final videoPostRepository =
+        VideoPostsRepositoryImpl(videosDatasource: LocalVideoDatasource());
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
-            lazy: false, create: (_) => DiscoverProvider()..loadNextPage())
+            lazy: false,
+            create: (_) =>
+                DiscoverProvider(videoRepository: videoPostRepository)
+                  ..loadNextPage())
       ],
       child: MaterialApp(
         title: 'TokTik',
